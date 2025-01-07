02b3009e4a1848f922ea26bfe034750fc6ff59c6
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-01-06 21:51:41 -0400

    Update notificationService.js

--- notificationService.js
+++ notificationService.js
@@ -9,7 +9,7 @@ self.addEventListener('notificationclick', function(event) {
             // Verifica se há alguma guia aberta
             for (let client of clientList) {
                 // Verifica se a guia já está aberta e se a URL corresponde
-                if (client.url === 'http://127.0.0.1:5500/index.html') {
+                if (client.url === 'https://telmopersch123.github.io/Alarme-Simples/') {
                     return client.focus(); // Foca na guia existente
                 }
             }
